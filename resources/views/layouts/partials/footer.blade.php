8e46d64ca26825e471629d5516b0fb795772b86b
Jesus <jbustoro16dw@ikzubirimanteo.com>
2017-10-06 21:55:04 +0200

    Initial commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <div id="author">Copyright © 2017-2018 by Jesus Busto Romero</div>
+</footer>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="home">
+        <!-- <img src="https://media.giphy.com/media/3o8dFwze0Tz8umFQhW/source.gif">-->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img class="img-responsive center-block" src="https://media.giphy.com/media/yoJC2wqc7suVTag7i8/giphy.gif">
+                </div>
+
+                <div class="item">
+                    <img class="img-responsive center-block" src="http://i.imgur.com/qhMbkGi.jpg">
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <div id="estado">
+            <h2>In progress...</h2>
+        </div>
+    </div>
+@endsection
